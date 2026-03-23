79ca85168e583536b1cbbb4f7b15c53ab61ce054
Sajid Islam <sajnyc360@gmail.com>
2026-03-23 18:34:00 -0400

    created game component and moved turn logic to game component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import { useState } from "react";
 
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (
+            squares[a] &&
+            squares[a] === squares[b] &&
+            squares[a] === squares[c]
+        ) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
 function Square({ value, onSquareClick }) {
     return (
         <>
@@ -10,9 +34,17 @@ function Square({ value, onSquareClick }) {
     );
 }
 
-export default function Board() {
-    const [xIsNext, setXIsNext] = useState(true);
-    const [squares, setSquares] = useState(Array(9).fill(null));
+function Board({ xIsNext, squares, onPlay }) {
+    function handleClick(i) {
+        if (squares[i] || calculateWinner(squares)) return;
+        const nextSquares = squares.slice();
+        if (xIsNext) {
+            nextSquares[i] = "X";
+        } else {
+            nextSquares[i] = "O";
+        }
+        onPlay(nextSquares);
+    }
 
     const winner = calculateWinner(squares);
     let status;
@@ -22,17 +54,6 @@ export default function Board() {
         status = "Next player: " + (xIsNext ? "X" : "O");
     }
 
-    function handleClick(i) {
-        if (squares[i] || calculateWinner(squares)) return;
-        const nextSquares = squares.slice();
-        if (xIsNext) {
-            nextSquares[i] = "X";
-        } else {
-            nextSquares[i] = "O";
-        }
-        setSquares(nextSquares);
-        setXIsNext(!xIsNext);
-    }
     return (
         <>
             <div className="status">{status}</div>
@@ -84,26 +105,27 @@ export default function Board() {
     );
 }
 
-function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (
-            squares[a] &&
-            squares[a] === squares[b] &&
-            squares[a] === squares[c]
-        ) {
-            return squares[a];
-        }
+export default function Game() {
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(0).fill(null)]);
+    const currentSquares = history[history.length - 1];
+
+    function handlePlay(nextSquares) {
+        setHistory([...history, nextSquares]);
+        setXIsNext(!xIsNext);
     }
-    return null;
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board
+                    xIsNext={xIsNext}
+                    squares={currentSquares}
+                    onPlay={handlePlay}
+                />
+            </div>
+            <div className="game-info">
+                <ol></ol>
+            </div>
+        </div>
+    );
 }
